8df279c906bc9ce8e54d164922b105269a54b445
Ruy <ruyoliv@hotmail.com>
2022-07-27 10:01:48 -0400

    nova versão

--- ex8_mud_tamanho_imagens.py
+++ ex8_mud_tamanho_imagens.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 path = r'.\imagens\frutas.png' 
 img = cv2.imread(path)
  
-metade = cv2.resize(img, (0, 0), fx = 0.4, fy = 0.4)
+metade = cv2.resize(img, (0, 0), fx = 0.5, fy = 0.5)
 maior = cv2.resize(img, (1050, 1610))
  
 stretch_near = cv2.resize(img, (780, 540),
@@ -18,7 +18,7 @@ stretch_near = cv2.resize(img, (780, 540),
 Titles =["Original", "Metade", "Maior", "Interpolação"]
 images =[img, metade, maior, stretch_near]
 count = 4
- 
+  
 for i in range(count):
     plt.subplot(2, 2, i + 1)
     plt.title(Titles[i])
